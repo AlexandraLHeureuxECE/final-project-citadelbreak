6f87aa2a1efe3180b351657af71211066afc2979
Femi Odujinrin <oodujin@uwo.ca>
2025-03-30 17:09:06 -0400

    Character Selection Added & Level 1 Complete (may add more to it though)

---
+++ Assets/Scripts/LoadCharacter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class LoadCharacter : MonoBehaviour
+{
+    void Awake()
+    {
+        LoadCharacterAtStartup();
+    }
+
+    void LoadCharacterAtStartup()
+    {
+        int selectedCharacter = PlayerPrefs.GetInt("selectedCharacter", 0);
+        GameObject player = GameObject.Find("Player");
+
+        if (player == null)
+        {
+            Debug.LogError("No GameObject named 'Player' found.");
+            return;
+        }
+
+        string[] characterNames = { "Male Character", "Female Character" };
+
+        for (int i = 0; i < characterNames.Length; i++)
+        {
+            Transform child = player.transform.Find(characterNames[i]);
+            if (child != null)
+            {
+                bool isActive = (i == selectedCharacter);
+                child.gameObject.SetActive(isActive);
+            }
+        }
+    }
+
+}
