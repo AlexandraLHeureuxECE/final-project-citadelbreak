375658fc582389f6702d6173b311207c9ed7ea98
Femi Odujinrin <oodujin@uwo.ca>
2025-04-06 17:20:49 -0400

    Fixed Issue with dialogue box not showing up with different aspect ratios & Each level now has exit level gate

--- Assets/Scripts/LevelChanger.cs
+++ Assets/Scripts/LevelChanger.cs
@@ -3,8 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class LevelChanger : MonoBehaviour
 {
-    [SerializeField] private string sceneToLoad;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
